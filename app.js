36cee2cf4af59dcdedc3e17cf1a3ef74d335c9fb
_Dewelg_ <_(dewel713@gmail.com_)>
2022-09-24 21:11:03 -0400

    almost done

--- app.js
+++ app.js
@@ -24,4 +24,78 @@ function checkAge(userName, age){
     }
 }
 let userName = 'Dewel';
-checkAge(`${userName}`, 17);
+checkAge(`${userName}`, 18);
+
+//Exercise3
+function quadrant(x,y){
+    if (x > 0 && y > 0){
+        console.log("this is in quadrant one");
+    }
+
+    else if (x < 0 && y > 0){
+        console.log('This is in qudrant two');
+    }
+
+    else if (x < 0 && y < 0){
+        console.log('This is in qudrant three');
+    }
+
+    else if (x > 0 && y < 0){
+        console.log('This is in qudrant four');
+    }
+}
+
+let x = 4;
+let y = -2;
+
+quadrant(x,y);
+
+//Exercise4
+function checktrianglevalid(a, b, c){
+     return a + b > c || a + c > b || b + c > a
+}
+
+function triangle(a, b, c){
+    let checktriangle = checktrianglevalid(a, b, c);
+    if (checktriangle){
+        if (a == b || b == c){
+            return('Equlaterial triangle');
+        } 
+        else if (a == b || b != c || a == c){
+            return('Isosceles triangles');
+        } 
+        else {
+            return('Not vaild triangle');
+        }
+
+    }
+
+    
+}
+
+console.log(triangle(2, 2, 2));
+console.log(triangle(2, 3, 2));
+
+//exersice5
+// need to get input of how many days left
+// see how much data is left
+// see how much they used data
+//then calculate there average daily use
+//the calcualte high low or medium usage 
+
+
+function dataUsage(day){ 
+    let daysDue = 30;
+    
+    if (daysDue > day){
+        let product = daysDue - day;
+        console.log(`${day}` + ' days used ' + `${product}` + ' days left');
+    } 
+    else if (daysDue <= day){
+        return('No more days left for data');
+    }
+ 
+   
+}
+
+console.log(dataUsage(15));
